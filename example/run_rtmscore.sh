b71569593d753928a73b63bf3c9f851af23610f8
William Schueller <william.schueller@gmail.com>
2025-03-21 17:03:39 +0100

    testing only 3/4 example scripts

--- example/run_rtmscore.sh
+++ example/run_rtmscore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # input is protein (needs to be converted to pocket)
-python rtmscore.py -p ./1qkt_p.pdb -l ./1qkt_decoys.sdf -rl ./1qkt_l.sdf -gen_pocket -c 10.0 -m ../trained_models/rtmscore_model1.pth
+#python rtmscore.py -p ./1qkt_p.pdb -l ./1qkt_decoys.sdf -rl ./1qkt_l.sdf -gen_pocket -c 10.0 -m ../trained_models/rtmscore_model1.pth
 
 # input is pocket
 python rtmscore.py -p ./1qkt_p_pocket_10.0.pdb -l ./1qkt_decoys.sdf -m ../trained_models/rtmscore_model1.pth
